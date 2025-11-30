2f8639581e5c07f8705ab036ec074734bcbe0bde
Akanksha <128801544+akanksha-th@users.noreply.github.com>
2025-11-30 14:11:10 +0530

    Add ModelFactory and count_parameters to exports

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,18 @@
 from .interfaces import Config, BasePipeline
 from .data import get_transforms, get_datasets, get_dataloaders, seed_everything
+from .model import ModelFactory, count_parameters
 
 __all__ = [
   "Config",
   "BasePipeline",
+  
   "get_transforms", 
   "get_datasets", 
   "get_dataloaders", 
-  "seed_everything"
+  "seed_everything",
+  
+  "ModelFactory", 
+  "count_parameters"
 ]
+
 
